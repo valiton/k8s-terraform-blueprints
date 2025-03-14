ddd55c6d05dfb22ac1005ea5c875c7f8961050aa
werner_m <mirko.werner@valiton.com>
2025-03-14 15:06:22 +0100

    fix eks_managed_node_groups

--- terraform/aws/base/main.tf
+++ terraform/aws/base/main.tf
@@ -114,7 +114,7 @@ locals {
       aws_region                  = local.region
       aws_account_id              = data.aws_caller_identity.current.account_id
       aws_vpc_id                  = module.vpc.vpc_id
-      base_nodepool_labels        = yamlencode(module.eks.eks_managed_node_group["base_eks_node"].node_group_labels)
+      base_nodepool_labels        = yamlencode(module.eks.eks_managed_node_groups["base_eks_node"].node_group_labels)
     },
     {
       addons_repo_url      = local.gitops_addons_url
